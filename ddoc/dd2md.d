4475498c40b72b4301ab24eb69b5620d6468b72f
k3_kaimu <enjouzensyou.boinc@gmail.com>
2013-08-12 22:05:54 +0900

    dd2md.dの更新

--- ddoc/dd2md.d
+++ ddoc/dd2md.d
@@ -22,29 +22,7 @@ import std.random;
 import std.format;
 
 
-enum markdown_ddoc = 
-`B =     **$0**
-I =     *$0*
-S =     ~~$0~~
-
-OL_1 =    <ol type="1">$0</ol>
-OL_a =  <ol type="a">$0</ol>
-OL_A =  <ol type="A">$0</ol>
-OL_i =  <ol type="i">$0</ol>
-OL_I =  <ol type="I">$0</ol>
-UL_none = <ul style="list-style-type: none">$0</ul>
-UL_disc = <ul style="list-style-type: disc">$0</ul>
-UL_circle = <ul style="list-style-type: circle">$0</ul>
-UL_square = <ul style="list-style-type: square">$0</ul>
-LINK =  [$0]($0)
-LINK2 = [$+]($1)
-ANCHOR = <a name = "$1">$+</a>
-D_CODE =
-~~~~d
-$0
-~~~~
-DDOC = $(BODY)
-IDENTITY = $0`;
+enum markdown_ddoc = import("markdown.ddoc");
 
 
 void main(string[] args)
@@ -59,16 +37,14 @@ void main(string[] args)
 
     // markdown.ddocをtempDirに出力
     std.file.write(markdownDdocPath, markdown_ddoc);
+    scope(exit) std.file.remove(markdownDdocPath);
 
     // dmdでddからmdファイルの生成
     auto app = appender!string();
     app.formattedWrite("dmd -c -o- -D %s %s -Df%s", markdownDdocPath, input, dmdOutputFilePath);
     executeShell(app.data);
+    scope(exit) std.file.remove(dmdOutputFilePath);
 
     // 出力されたファイルの<(&lt;), >(&gt;), &(&amp;)を置換し、そのままターゲットに出力
     std.file.write(output, std.file.readText(dmdOutputFilePath).replace("&lt;", "<").replace("&gt;", ">").replace("&amp;", "&"));
-
-    // いらないファイルの削除
-    std.file.remove(markdownDdocPath);
-    std.file.remove(dmdOutputFilePath);
 }
